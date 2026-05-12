34bf8a32b15df932a57b0a68a84e249036bcd725
Edo <edoardo@loomery.com>
2026-05-12 11:27:39 +0100

    fix: npm fix, remove figma temporary components

--- app/showcase/page.jsx
+++
@@ -1,9 +0,0 @@
-import { ShowcaseScreen } from '../../screens/Showcase/ShowcaseScreen';
-
-export const metadata = {
-  title: 'Component showcase | UAL Welcome Week',
-};
-
-export default function ShowcasePage() {
-  return <ShowcaseScreen />;
-}
